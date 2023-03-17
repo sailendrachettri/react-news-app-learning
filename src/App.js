f9249d2edf26a99bff3e5fd95655540bc519db44
sailendrachettri <sailendra9083@gmail.com>
2023-03-17 13:04:34 +0530

    infinite scroll

--- src/App.js
+++ src/App.js
@@ -7,19 +7,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default class App extends Component {
   render() {
+    let pageSize = 6;
     return (
       <>
         <Router>
           <Navbar />
 
           <Routes>
-            <Route path='/' element={<News key="general" pageSize={9} category="general" />} />
-            <Route path='/entertainment' element={<News key="entertainment" pageSize={9} category="entertainment" />} />
-            <Route path='/sports' element={<News key="sports" pageSize={9} category="sports" />} />
-            <Route path='/science' element={<News key="science" pageSize={9} category="science" />} />
-            <Route path='/health' element={<News key="health" pageSize={9} category="health" />} />
-            <Route path='/business' element={<News key="business" pageSize={9} category="business" />} />
-            <Route path='/technology' element={<News key="technology" pageSize={9} category="technology" />} />
+            <Route path='/' element={<News key="general" pageSize={pageSize} category="general" />} />
+            <Route path='/entertainment' element={<News key="entertainment" pageSize={pageSize} category="entertainment" />} />
+            <Route path='/sports' element={<News key="sports" pageSize={pageSize} category="sports" />} />
+            <Route path='/science' element={<News key="science" pageSize={pageSize} category="science" />} />
+            <Route path='/health' element={<News key="health" pageSize={pageSize} category="health" />} />
+            <Route path='/business' element={<News key="business" pageSize={pageSize} category="business" />} />
+            <Route path='/technology' element={<News key="technology" pageSize={pageSize} category="technology" />} />
 
           </Routes>
 
--- src/components/News.js
+++ src/components/News.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export class News extends Component {
     static defaultProps = {
         country: "in",
-        pageSize: 9,
+        pageSize: 6,
         category: "general"
     }
 
@@ -26,7 +27,8 @@ export class News extends Component {
         this.state = {
             articles: [],
             page: 1,
-            loading: false
+            loading: true,
+            totalResults: 0
         }
         document.title = `${this.props.category === 'general' ? "News Monkey - get the latest news for free" : this.captializeFirstLetter(this.props.category)} latest news - NewsMonkey`
     }
@@ -49,16 +51,17 @@ export class News extends Component {
         this.updateNews()
     }
 
-    goToNextPage = async () => {
+    fetchMoreData = async () => {
         this.setState({ page: this.state.page + 1 })
-        this.updateNews()
-        console.log(this.state.articles);
-    }
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=3104946bd90a4c738d1a1e4d7dc35012&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
 
-    goToPreviousPage = async () => {
-        this.setState({ page: this.state.page - 1 })
-        this.updateNews()
-    }
+        this.setState({
+            articles: this.state.articles.concat(parsedData.articles),
+            totalResults: parsedData.totalResults
+        })
+    };
 
     render() {
         return (
@@ -66,19 +69,25 @@ export class News extends Component {
                 <div className="container my-3">
                     <h2 className='my-3 fontSensSerif'> NewsMonkey - Top {this.captializeFirstLetter(this.props.category)} headlines </h2>
                     {this.state.loading && <Spinner />}
-                    <div className="row">
-                        {
-                            (!this.state.loading) && this.state.articles.map((element) => {
-                                return <div div className="col-md-4" key={element.imageUrl}>
-                                    <NewsItem title={element.title ? element.title.slice(0, 45) : "Not available"} description={element.description ? element.description.slice(0, 88) : "Not available"} imageUrl={!element.urlToImage ? "https://thumbs.dreamstime.com/b/news-newspapers-folded-stacked-word-wooden-block-puzzle-dice-concept-newspaper-media-press-release-42301371.jpg" : element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} />
-                                </div>
-                            })
-                        }
-                    </div>
-                    <div className="container d-flex justify-content-between my-3">
-                        <button type='buttom' className='btn btn-dark' onClick={this.goToPreviousPage} disabled={this.state.page <= 1}>&larr; Previous</button>
-                        <button type='buttom' className='btn btn-dark' onClick={this.goToNextPage} disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)}>Next &rarr; </button>
-                    </div>
+                    <InfiniteScroll
+                        dataLength={this.state.articles.length}
+                        next={this.fetchMoreData}
+                        hasMore={this.state.articles.length !== this.state.totalResults}
+                        loader={<Spinner />}
+                    >
+                        <div className="container">
+                            <div className="row">
+                                {
+                                    this.state.articles.map((element) => {
+                                        return <div div className="col-md-4" key={element.imageUrl}>
+                                            <NewsItem title={element.title ? element.title.slice(0, 45) : "Not available"} description={element.description ? element.description.slice(0, 88) : "Not available"} imageUrl={!element.urlToImage ? "https://thumbs.dreamstime.com/b/news-newspapers-folded-stacked-word-wooden-block-puzzle-dice-concept-newspaper-media-press-release-42301371.jpg" : element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} />
+                                        </div>
+                                    })
+                                }
+                            </div>
+                        </div>
+                    </InfiniteScroll>
+
                 </div>
             </>
         )
